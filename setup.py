4fd5adb0bb2ea519a429cbb550c735894cfeb375
cdfarrow <cdfarrow@users.noreply.github.com>
2022-05-31 14:58:45 +1200

    Update setup files for latest best practice
    Move static configuration to setup.cfg; add pyproject.toml (tool-agnostic installation dependency list)

--- setup.py
+++ setup.py
@@ -1,44 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# --- Basic Library Information ------
-name = 'flexlibs'
-version = '1.0.3'
-# ------------------------------------
-
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE.txt') as f:
-    license = f.read()
-
-setup(
-    name=name,
-    version=version,
-    description='Library for accessing FieldWorks Language Explorer projects',
-    long_description=readme,
-    author='Craig Farrow',
-    author_email='flextoolshelp@gmail.com',
-    url='https://github.com/cdfarrow/flexlibs',
-    license=license,
-    platforms=['Windows', 'Linux'],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
-    packages=find_packages(include=('flexlibs','flexlibs.*')),
-    package_data = {'': ['*.dll']},
-    install_requires=[
-        'pythonnet>=2.0.0',
-        'future;python_version=="2.7"',
-        'sphinx'
-    ],
-    include_package_data=True,
-    classifiers = [
-        'Programming Language :: Python 2.7',
-        'Programming Language :: Python 3.5',
-        'Programming Language :: Python 3.6',
-        'Programming Language :: Python 3.7',
-        'Topic :: Text Processing :: Linguistic',
-        'Topic :: Database',
-        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
-    ],
-)
+setup()

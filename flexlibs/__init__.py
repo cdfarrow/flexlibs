f1a36f1f98640cae47fbf9888024278a629890f6
cdfarrow <cdfarrow@users.noreply.github.com>
2022-06-03 16:50:25 +1200

    Update build files for new structure
    From pip v21.1 setup.py is not required even for development/editable mode [https://setuptools.pypa.io/en/latest/userguide/quickstart.html]

---
+++ flexlibs/__init__.py
@@ -0,0 +1,35 @@
+#----------------------------------------------------------------------------
+# Name:         __init__.py
+# Purpose:      flexlibs provides a Python interface to FLEx project data 
+#               via the Fieldworks Language and Culture Model (LCM).
+#
+#----------------------------------------------------------------------------
+
+# Define exported classes, etc. at the top level of the package
+
+from .code.FLExInit import (
+    Initialize, 
+    Cleanup,
+    )
+    
+from .code.FLExGlobals import (
+    FWCodeDir, 
+    FWProjectsDir, 
+    FWShortVersion, 
+    FWLongVersion,
+    APIHelpFile,
+    )
+
+from .code.FLExProject import (
+    AllProjectNames,
+    FLExProject,
+    FP_FileLockedError,
+    FP_FileNotFoundError,
+    FP_MigrationRequired, 
+    FP_NullParameterError, 
+    FP_ParameterError, 
+    FP_ProjectError, 
+    FP_ReadOnlyError, 
+    FP_RuntimeError, 
+    FP_WritingSystemError,
+    )
--- flexlibs/code/__init__.py
+++
@@ -1,7 +0,0 @@
-#----------------------------------------------------------------------------
-# Name:         __init__.py
-# Purpose:      flexlibs provides a Python interface to FLEx project data 
-#               via the Fieldworks Language and Culture Model (LCM).
-#
-#----------------------------------------------------------------------------
-

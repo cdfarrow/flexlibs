6903398c253734a5ac2a3ec0a05610a8075e9633
cdfarrow <cdfarrow@users.noreply.github.com>
2022-06-15 13:55:37 +1200

    Updated examples for Py3 & latest flexlibs API

---
+++ flexlibs/examples/demo_lexicon.py
@@ -0,0 +1,106 @@
+# -*- coding: cp1252 -*-
+
+#   demo_lexicon.py
+#
+#   Tests and demonstrates working with the FieldWorks lexicon.
+#
+#   Platforms: Python .NET and IronPython
+#
+#   Copyright Craig Farrow, 2008 - 2022
+#
+
+import sys
+
+from flexlibs import FLExInitialize, FLExCleanup
+from flexlibs import FLExProject, FP_ProjectError
+
+#============ Configurables ===============
+
+# Project to use
+TEST_PROJECT = "__flexlibs_testing"
+
+# Maximum number of entries to print out; use
+# a very big number for the whole lexicon.
+LexiconMaxEntries = 20
+
+# Enable writing tests
+WriteTests = False
+
+# Custom field name
+CUSTOM_FIELD = r"SenseFlags"
+      
+#--------------------------------------------------------------------
+def reportLexicalEntries(project):
+
+    ftflagsFlid = project.LexiconGetSenseCustomFieldNamed(CUSTOM_FIELD)
+    
+    print("Opened project %s." % project.ProjectName())
+
+    print("Listing up to %d lexical entries:" % LexiconMaxEntries)
+    print()
+
+    # Scan the lexicon (unordered), printing some of the data
+    # using Standard Format markers.
+    # (Uses a slice to only print a portion.)
+    for e in list(project.LexiconAllEntries())[:LexiconMaxEntries]:
+        print("\lx", project.LexiconGetLexemeForm(e))
+        print("\lc", project.LexiconGetCitationForm(e))
+        for sense in e.SensesOS :
+            print("\ge", project.LexiconGetSenseGloss(sense))
+            print("\pos", project.LexiconGetSensePOS(sense))
+            print("\def", project.LexiconGetSenseDefinition(sense))
+            if WriteTests and ftflagsFlid:
+                flags = project.LexiconGetFieldText(sense, ftflagsFlid)
+                print("FTFlags", flags)
+                # CHANGES the project
+                if flags:
+                    project.LexiconAddTagToField(sense, ftflagsFlid, "tag-1")
+                else:
+                    project.LexiconSetFieldText(sense, ftflagsFlid, "FLAG!")
+                flags = project.LexiconGetFieldText(sense, ftflagsFlid)
+                print("New FTFlags", flags)
+            if WriteTests:
+                if project.LexiconGetSenseGloss(sense) == "Example Gloss":
+                    # CHANGES the project
+                    project.LexiconSetSenseGloss(sense, "Changed Gloss")
+                    print("New Gloss", project.LexiconGetSenseGloss(sense))
+
+            for example in sense.ExamplesOS:
+                ex = project.LexiconGetExample(example)
+                print("\ex", ex)
+                if WriteTests and not ex:
+                    # CHANGES the project
+                    print("Setting example")
+                    project.LexiconSetExample(example,
+                                             "You should have an example sentence"
+                                             )
+
+        print()
+
+
+#--------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+
+    FLExInitialize()
+    
+    project = FLExProject()
+
+    try:
+        project.OpenProject(projectName = TEST_PROJECT,
+                            writeEnabled = WriteTests)
+    except FP_ProjectError as e:
+        print("OpenProject failed!")
+        print(e.message)
+        FLExCleanup()
+        sys.exit(1)
+
+    reportLexicalEntries(project)
+    
+    # Clean-up
+    del project
+    
+    FLExCleanup()
+    
+    
---
+++ flexlibs/examples/demo_openproject.py
@@ -0,0 +1,99 @@
+# -*- coding: cp1252 -*-
+
+#   demo_openproject.py
+#
+#   Tests and demonstrates access to a FieldWorks project.
+#
+#   Platforms: Python .NET and IronPython
+#
+#   Copyright Craig Farrow, 2008 - 2022
+#
+
+import sys
+
+from flexlibs import FLExInitialize, FLExCleanup
+from flexlibs import FLExProject, FP_ProjectError
+
+
+#============ Configurables ===============
+
+# Project to use
+TEST_PROJECT = r"__flexlibs_testing"
+
+
+def reportBasicInfo(project):
+    # Global things in the Language Project
+    
+    print("Information on project %s:" % project.ProjectName())
+
+    #============ General Settings =======
+    print("Last modified:", project.GetDateLastModified())
+
+    posList = project.GetPartsOfSpeech()
+    print("There are %i Parts of Speech in this project:" % len(posList))
+    for i in posList:
+        print("\t", i)
+    print()
+
+
+    #============ Writing Systems ============
+
+    # The names of WS associated with this DB. Sorted and with no duplicates.
+
+    wsList = project.GetWritingSystems()
+    print("There are %i writing Systems in this project: (Language Tag, Handle)"
+          % len(wsList))
+    print()
+    for x in wsList:
+        name, langTag, handle, isVern = x
+        print("\t", name)
+        print("\t\t", langTag, handle)
+        print("\t\t", "(Vernacular)" if isVern else "(Analysis)")
+    print()
+
+    # A tuple of (language-tag, display-name)
+    print("\tDefault vernacular WS = %s; %s" % project.GetDefaultVernacularWS())
+    print("\tDefault analysis WS   = %s; %s" % project.GetDefaultAnalysisWS())
+    print()
+
+    #============= Lexicon ================
+    print("Custom Fields:")
+    print("\tEntry level:")
+    for cf in project.LexiconGetEntryCustomFields():
+        # Tuple of flid and user-defined name:
+        print("\t\t%s (%s)" % (cf[1], cf[0]))
+    print()
+
+    print("\tSense level:")
+    for cf in project.LexiconGetSenseCustomFields():
+        print("\t\t%s (%s)" % (cf[1], cf[0]))
+    print()
+
+    print("The lexicon contains %d entries" % project.LexiconNumberOfEntries())
+    
+    print("The text corpus contains %d texts" % project.TextsNumberOfTexts())
+
+# -------------------------------------------------------------------    
+if __name__ == "__main__":
+
+    FLExInitialize()
+    
+    project = FLExProject()
+
+    try:
+        project.OpenProject(projectName = TEST_PROJECT,
+                            writeEnabled = False)
+    except FP_ProjectError as e:
+        print("OpenProject failed!")
+        print(e.message)
+        FLExCleanup()
+        sys.exit(1)
+        
+    reportBasicInfo(project)
+    
+    # Clean-up
+    del project
+    
+    FLExCleanup()
+    
+    

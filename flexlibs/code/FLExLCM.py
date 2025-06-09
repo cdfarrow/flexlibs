50b5292670ba209de7e9e4bb27a764e97ef480a3
cdfarrow <cdfarrow@users.noreply.github.com>
2025-06-09 17:42:12 +1100

    #14 and #48 Project list shows ghost projects
    Check for fwdata file not just the directory existing

--- flexlibs/code/FLExLCM.py
+++ flexlibs/code/FLExLCM.py
@@ -57,9 +57,14 @@ def GetListOfProjects():
     projectsPath = FwDirectoryFinder.ProjectsDirectory
     objs = os.listdir(str(projectsPath))
     projectList = []
-    for f in objs:
-        if os.path.isdir(os.path.join(projectsPath, f)):
-            projectList.append(f)
+    for dirname in objs:
+        # FieldWorks can leave ghost directories, so we test 
+        # for the fwdata file, not just the directory. (Issue #48)
+        suffix = LcmFileHelper.ksFwDataXmlFileExtension 
+        if os.path.isfile(os.path.join(projectsPath, 
+                                       dirname, 
+                                       dirname+suffix)):
+            projectList.append(dirname)
     return sorted(projectList)
 
 #-----------------------------------------------------------
